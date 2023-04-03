d22ad347e532cd417bd9a9e2ad0c13c62faf2264
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-04-03 22:18:05 +0200

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,25 +1,66 @@
-export type SegmentUserTraits = {
-  [key: string]: string;
-};
-
-export type SourceId = {
-  source_id: string;
-};
-
-export type VoucherifyCustomer =
-  | SegmentUserTraits
-  | {
-      source_id?: SourceId;
-    };
-
 export type ProfileResponse = {
-  allSegmentIds: string[];
-  hasMore: boolean;
-  next?: string;
+    allSegmentIds: string[];
+    hasMore?: boolean;
+    next?: string;
 };
 
 export type AllSegmentIdsResponse = {
-  allSegmentIds: string[];
-  hasMore: boolean;
-  next: string;
+    allSegmentIds: string[];
+    hasMore?: boolean;
+    next?: string;
 };
+
+export interface SegmentUserTraits {
+    name?: string;
+    firstName?: string;
+    first_name?: string;
+    lastName?: string;
+    last_name?: string;
+    email?: string;
+    description?: string;
+    address?: {
+        city?: string;
+        state?: string;
+        postalCode?: string;
+        postal_code?: string;
+        street?: string;
+        line_1?: string;
+        country?: string;
+    };
+    city?: string;
+    state?: string;
+    postalCode?: string;
+    postal_code?: string;
+    street?: string;
+    line_1?: string;
+    country?: string;
+    phone?: string;
+    birthdate?: string;
+    metadata?: object;
+}
+
+export type SourceId = {
+    source_id: string;
+};
+
+export interface Address {
+    city?: string;
+    state?: string;
+    postal_code?: string;
+    line_1?: string;
+    country?: string;
+}
+
+export interface VoucherifyCustomer {
+    name?: string | undefined;
+    source_id: SourceId | string;
+    email?: string | undefined;
+    description?: string | undefined;
+    address?: Address;
+    phone?: string | undefined;
+    birthdate?: string | null;
+    metadata?: object;
+    system_metadata?: object;
+}
+
+
