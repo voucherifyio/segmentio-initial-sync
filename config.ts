7f22e00ba2e11a31c15355c1681b1a7dc142f197
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-04-03 12:21:43 +0200

    update name of function that throws error

--- config.ts
+++ config.ts
@@ -1,13 +1,13 @@
 import { config } from 'dotenv';
 config();
 
-export const SEGMENT_ACCESS_TOKEN = process.env.SEGMENT_ACCESS_TOKEN ? process.env.SEGMENT_ACCESS_TOKEN : missing('SEGMENT_ACCESS_TOKEN');
-export const SEGMENT_SPACE_ID = process.env.SEGMENT_SPACE_ID ? process.env.SEGMENT_SPACE_ID :  missing('SEGMENT_SPACE_ID');
-export const SEGMENT_REQUEST_LIMIT = process.env.SEGMENT_REQUEST_LIMIT ? process.env.SEGMENT_REQUEST_LIMIT : missing('SEGMENT_REQUEST_LIMIT');
-export const SEGMENT_TRAITS_LIMIT = process.env.SEGMENT_TRAITS_LIMIT ? process.env.SEGMENT_TRAITS_LIMIT : missing('SEGMENT_TRAITS_LIMIT');
-export const VOUCHERIFY_APPLICATION_ID = process.env.VOUCHERIFY_APPLICATION_ID ? process.env.VOUCHERIFY_APPLICATION_ID : missing('VOUCHERIFY_APPLICATION_ID');
-export const VOUCHERIFY_SECRET_KEY = process.env.VOUCHERIFY_SECRET_KEY ? process.env.VOUCHERIFY_SECRET_KEY : missing('VOUCHERIFY_SECRET_KEY');
+export const SEGMENT_ACCESS_TOKEN = process.env.SEGMENT_ACCESS_TOKEN ? process.env.SEGMENT_ACCESS_TOKEN : throwMissingVariableError('SEGMENT_ACCESS_TOKEN');
+export const SEGMENT_SPACE_ID = process.env.SEGMENT_SPACE_ID ? process.env.SEGMENT_SPACE_ID :  throwMissingVariableError('SEGMENT_SPACE_ID');
+export const SEGMENT_REQUEST_LIMIT = process.env.SEGMENT_REQUEST_LIMIT ? process.env.SEGMENT_REQUEST_LIMIT : throwMissingVariableError('SEGMENT_REQUEST_LIMIT');
+export const SEGMENT_TRAITS_LIMIT = process.env.SEGMENT_TRAITS_LIMIT ? process.env.SEGMENT_TRAITS_LIMIT : throwMissingVariableError('SEGMENT_TRAITS_LIMIT');
+export const VOUCHERIFY_APPLICATION_ID = process.env.VOUCHERIFY_APPLICATION_ID ? process.env.VOUCHERIFY_APPLICATION_ID : throwMissingVariableError('VOUCHERIFY_APPLICATION_ID');
+export const VOUCHERIFY_SECRET_KEY = process.env.VOUCHERIFY_SECRET_KEY ? process.env.VOUCHERIFY_SECRET_KEY : throwMissingVariableError('VOUCHERIFY_SECRET_KEY');
 
-function missing(name: string): never {
+function throwMissingVariableError(name: string): never {
   throw new Error(`Invalid value for environment variable: ${name}`);
 }
